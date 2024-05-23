aee8cc06e7e29868d17767db4f68bcf1eb0c3bca
hikmetcnli <hikmetcnli@hotmail.com>
2024-05-23 14:47:41 +0300

    People

--- CORECRUD/Controllers/PeopleController.cs
+++ CORECRUD/Controllers/PeopleController.cs
@@ -1,6 +1,7 @@
 ﻿
 using CORECRUD.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace CORECRUD.Controllers
@@ -17,15 +18,41 @@ namespace CORECRUD.Controllers
         [HttpGet]
         public IActionResult New()
         {
-            var depList = c.Department_.ToList();
+            LoadDepartment();
 
-            return View();
+			return View();
         }
 		[HttpPost]
 		public IActionResult New(People people)
 		{
-			return View();
+            //var dep = c.Department_.Find(people.DepartmentID);
+            //people._Depart = dep;
+
+
+			c.People_.Add(people);
+            c.SaveChanges();
+
+			return RedirectToAction("Index","People");
 		}
+
+        [NonAction]
+        public void LoadDepartment()
+        {
+            var departmanList = new List<SelectListItem>();
+            //Departman Listesini doldur
+            foreach (var department in c.Department_.ToList())
+            {
+                departmanList.Add(new SelectListItem
+                {
+                    Value = department.DepartmentID.ToString(),
+                    Text = department.DepartmentName.ToString()
+                });
+			}
+
+            ViewBag.Department = departmanList;
+        }
+
+
 
 	}
 }
